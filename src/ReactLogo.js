2eb845ccd2ce0e63d00389b9965f646336eeb93f
harryzhux <harryzhux@gmail.com>
2017-07-06 23:57:39 -0700

    add QR

--- src/ReactLogo.js
+++ src/ReactLogo.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import qrimg from './HarryQRCode.jpg';
 import './ReactLogo.css';
 
 class ReactLogo extends Component {
   render() {
     return (
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Flip-Match Card game with React</h2>
+        <h2>
+	    <img src={logo} className="App-logo" alt="logo" />
+            Flip-Match Card game with React
+            <img src={qrimg} className="App-QR" alt="QR" />
+	</h2>
       </div>
     );
   }
